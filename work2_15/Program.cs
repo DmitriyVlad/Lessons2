979463670528733ff3387684e380fb0869fee6cf
Dmitriy <timon@yahoo.com>
2022-12-07 11:32:38 +0300

    Задача 15

---
+++ work2_15/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 15
+
+int day = new Random().Next(1,7);
+if(day < 6)
+{
+    Console.WriteLine(day + " - это рабочий день");
+}
+else
+{
+    Console.WriteLine(day + " - это выходной!!!");
+}
