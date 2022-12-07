a11eb3406948a43aebde6136a29056d64ecfb98b
Dmitriy <timon@yahoo.com>
2022-12-07 11:27:03 +0300

    Задача 13

---
+++ work2_13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 13
+
+Console.WriteLine("Введите любое число...");
+string str = Console.ReadLine();
+int n = str.Length;
+if(n < 3)
+{
+    Console.WriteLine("В числе меньше трех цифр!");
+}
+else
+{
+Console.WriteLine("Третья цифра - " + str[2]);
+}
